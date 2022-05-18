1a33c432d306eafda3f2f584186ae7f63feff313
mancini-michael <mich.mancini06@gmail.com>
2022-05-18 21:09:01 +0200

    added homepage style

--- server/controllers/homepage.js
+++ server/controllers/homepage.js
@@ -1,9 +1,15 @@
+const fetch = require("node-fetch");
+
 module.exports = {
-  renderPage: (req, res) => {
-    res.render("homepage", {
-      title: "Homepage",
-      auth: req.isAuthenticated(),
-      user: req.session.passport.user,
-    });
+  renderPage: async (req, res) => {
+    await fetch("http://nginx/api/v1/post")
+      .then((result) => result.json())
+      .then((result) => {
+        res.render("homepage", {
+          title: "Homepage",
+          user: req.session.passport.user,
+          post: result,
+        });
+      });
   },
 };
--- server/public/js/script.js
+++ server/public/js/script.js
@@ -1,3 +1,32 @@
 $(document).ready(() => {
   $("footer .container-md span").html(`&copy; ${new Date().getFullYear()}`);
 });
+
+$(document).ready(() => {
+  $("#post").submit(async (event) => {
+    event.preventDefault();
+
+    const author = $("#author").val();
+    const description = $("#description").val();
+    const photo = $("#photo").val();
+
+    if (!description) {
+      $("#alert").append(
+        `<div class="alert alert-danger text-center">Aggiungere una descrizione.</div>`
+      );
+      return;
+    }
+
+    const post = { author, description, photo };
+
+    $.ajax({
+      url: "http://localhost:8080/api/v1/post",
+      type: "POST",
+      data: post,
+      success: (result) => {
+        alert("creato");
+        console.log(result);
+      },
+    });
+  });
+});
