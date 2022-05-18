43033dd7d64d8c6262251d438e062f0903eca2e6
mancini-michael <mich.mancini06@gmail.com>
2022-05-18 19:26:59 +0200

    added homepage form for post

--- server/controllers/homepage.js
+++ server/controllers/homepage.js
@@ -1,5 +1,9 @@
 module.exports = {
   renderPage: (req, res) => {
-    res.render("homepage", { title: "Homepage" });
+    res.render("homepage", {
+      title: "Homepage",
+      auth: req.isAuthenticated(),
+      user: req.session.passport.user,
+    });
   },
 };
